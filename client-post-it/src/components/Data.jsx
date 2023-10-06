d295c1e353364ef668d0006ad7deba01c6e3e0db
CarolinaInglethe <carolinainglethe@gmail.com>
2023-10-06 01:49:06 -0300

    Iniciando teste com a logica para exibir dados na tela

--- client-post-it/src/components/Data.jsx
+++ client-post-it/src/components/Data.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 function Data() {
+  const data = {
+    "exemplo1": ["nome lembrete ", "data"],
+    "exemplo2": ["nome lembrete 2", "data"]
+  }
+
     return (
         <div>
         <h2>Lista de lembretes</h2>
+        {
+          !data ? <p>Carregando..</p> : data.exemplo1.map((value,index) => (
+          <div key={index}> 
+            <p>{value}</p>
+          </div>))
+        }
       </div>
     )
 }
