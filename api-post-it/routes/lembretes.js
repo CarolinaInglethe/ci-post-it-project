9115ff83eac6e658123a9c753ca98b469668f201
CarolinaInglethe <carolinainglethe@gmail.com>
2023-10-07 01:15:36 -0300

    backend - Crio controller  onde as rotas irão fazer requisição ao banco

--- api-post-it/routes/lembretes.js
+++ api-post-it/routes/lembretes.js
@@ -1,7 +1,8 @@
 import express  from "express";
+import { getAll } from "../controllers/lembretes"
 
 const router = express.Router();
 
-router.get("/", getLembretes);
+router.get("/", getAll);
 
 export default router;
