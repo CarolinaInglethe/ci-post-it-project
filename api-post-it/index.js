ca9bfcc9606cc9167c872221def342e7bc06e450
CarolinaInglethe <carolinainglethe@gmail.com>
2023-10-07 01:20:46 -0300

    backend - O app ira usar rota criada , especificando getAll com /

--- api-post-it/index.js
+++ api-post-it/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import Routes from './routes/lembretes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors()); // evitar conflitos para rodar localmente.
+
+app.use('/', Routes);
 
 app.listen(8800);
